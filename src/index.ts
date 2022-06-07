fc4f1355f9e8fb87f415a319e5cb5891703598de
Krylov Sergey <ksv741@mail.ru>
2022-06-07 17:26:09 +0500

    work with webpack

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
+
+import './style.scss';
+
 console.log('Webpack with typescript');
+
+const a = 25;
+
+const Persone = {
+  name: 'huy',
+  age: 25,
+  skils: ['html', 'css', 'js']
+}
+
+console.log('Persone', Persone);
+
+const start = new Promise((res, rej) => {
+  console.log('ONE!');
+})
+
+start.then(() => {
+  console.log('DWO');
+})
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,15 +1,76 @@
 import * as path from 'path';
 import * as webpack from 'webpack';
-// in case you run into any typescript error when configuring `devServer`
-// import 'webpack-dev-server';
 
-const config: webpack.Configuration = {
-  context: path.resolve(__dirname, 'src'),
-  entry: './index.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name]-[hash].bundle.js',
-  },
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+// FIXME: type of any
+const config = (env: Record<string, any>, argv: Record<string, any>): webpack.Configuration => {
+
+  const isProd = argv.mode === 'production';
+
+  const getFilename = (ext: string): string => {
+    return `[name]${isProd ? '-[hash]' : ''}.${ext}`;
+  }
+
+  const getSourceMap = () => {
+    return isProd ? false : 'source-map'
+  }
+
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: ['core-js/stable', 'regenerator-runtime/runtime','./index.ts'],
+    devtool: getSourceMap(),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: getFilename('js'),
+      clean: true
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'src', '@core')
+      }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        minify: false,
+        template: './index.html'
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: "favicon.ico", to: "./" },
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: getFilename('css')
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.m?tsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ],
+    },
+  }
 };
 
 export default config;
